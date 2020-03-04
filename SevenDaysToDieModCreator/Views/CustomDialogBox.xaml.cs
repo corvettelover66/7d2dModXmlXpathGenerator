8aa98b45be98b574a93ebb8cf239fd801f77bc8a
jjones <jjonessams@gmail.com>
2020-03-04 12:46:12 +0100

    Fixed typo in the CustomTag Pop up window.

--- SevenDaysToDieModCreator/Views/CustomDialogBox.xaml.cs
+++ SevenDaysToDieModCreator/Views/CustomDialogBox.xaml.cs
@@ -19,7 +19,7 @@ namespace SevenDaysToDieModCreator.Views
             InitializeComponent();
             string defaultText = "Thank you for downloading the 7 days to die Mod Creator! " +
                 "Please input your custom tag now! This will be the top tag used in the xml output files. " +
-                "It is also the name used in the Output folder when saving and mobing the mod.\n\n" +
+                "It is also the name used in the Output folder when saving and moving the mod files.\n\n" +
                 "IMPORTANT: If you lose work check the log.txt in the Output folder. " +
                 "Any time you close the app or reset the object view, the xml that could be generated is output in that log. " +
                 "If you like the mod don't forget to drop an endorsment and tell your friends!";
